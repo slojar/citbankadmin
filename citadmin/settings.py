7b490d1b66ab24e9fa64427e092b1c71d560ec9b
slojararshavin <slojararshavin@gitlab.com>
2022-09-15 21:21:26 +0100

    add domain

--- citadmin/settings.py
+++ citadmin/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "2.56.213.151"]
+ALLOWED_HOSTS = ["127.0.0.1", "2.56.213.151", "admin.citmfb.com"]
 
 
 # Application definition
